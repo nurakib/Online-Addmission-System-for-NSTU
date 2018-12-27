7c7897f74067bc1f95ec4498abd0d77e1c873d65
Nayem Uddin <nurakib.nstu@gmail.com>
2018-12-27 15:45:43 +0600

    v 1.0

---
+++ faq.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8"/>
+  <title>Notice Page</title>
+  <link href="static/style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+  <div class="nav-bar">
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="notice.html">Notice</a></li>
+        <li><a href="faq.html">FAQ</a></li>
+        <li><a href="http://nstu.edu.bd/">NSTU Website</a></li>
+        <li><a href="login.php">Enrollment Login</a></li>
+      </ul>
+  </div>
+  <div class="faq-container">
+    <div id="wrapper">
+      <div class="content">
+                <div class="faqs">
+                    <h2>Address of NSTU</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                </div>
+
+                <div class="faqs">
+                    <h2>How many departments NSTU have?</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+                </div>
+
+                <div class="faqs">
+                    <h2>Is NSTU politics free?</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. <p>
+                </div>
+            </div>
+    </div>
+  </div>
+</body>
+<html>
---
+++ notice.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <title>Notice Page</title>
+    <link href="static/style.css" type="text/css" rel="stylesheet">
+  </head>
+  <body>
+  <div class="nav-bar">
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="notice.html">Notice</a></li>
+        <li><a href="faq.html">FAQ</a></li>
+        <li><a href="http://nstu.edu.bd/">NSTU Website</a></li>
+        <li><a href="login.php">Enrollment Login</a></li>
+      </ul>
+  </div>
+  <div class="notice-container">
+    <div id="wrapper">
+      <div class="content">
+                <div class="notices">
+                    <h2>Students who will not reach in time can't admit later</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                </div>
+
+                <div class="notices">
+                    <h2>Payment Total approximately 25000 BDT</h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                </div>
+
+                <div class="notices">
+                    <h2>Admission declared on 3 & 4 December 2017 </h2>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                </div>
+            </div>
+    </div>
+  </div>
+  </body>
+<html>
